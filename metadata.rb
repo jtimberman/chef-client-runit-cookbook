696e85f9288fd19ecaafcf24ec593fb8a1ecb85c
jtimberman <joshua@chef.io>
2015-02-26 16:11:18 -0700

    add repo urls

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,8 @@ license          'Apache 2.0'
 description      'Sets up chef-client as a runit-supervised service'
 version          '0.1.0'
 depends          'runit'
+
+unless defined?(Ridley::Chef::Cookbook::Metadata)
+  source_url       'https://github.com/jtimberman/chef-client-runit-cookbook'
+  issues_url       'https://github.com/jtimberman/chef-client-runit-cookbook/issues'
+end
